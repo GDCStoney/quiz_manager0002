f4fc1373cf76cc05b6420c5c9a2d706a55007dd0
gstoney2 <gstoney2@ford.com>
2022-02-07 17:54:00 +0000

    Added core interface functionality prior to implementing the REST API call to register a new user

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
 
 import AuthService from "../../Services/auth-service";
+import Form from "react-validation/src/components/form";
+import Input from "react-validation/src/components/input";
+import Button from "react-validation/src/components/button";
+
+const required = value => {
+    if (!value) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This field is required!
+            </div>
+        );
+    }
+};
 
 export default class Register extends Component {
     constructor(props) {
         super(props);
 
         this.handleRegister = this.handleRegister.bind(this);
+
+        this.onChangeRoleId = this.onChangeRoleId.bind(this);
+        this.onChangeFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
 
         this.state = {
             roleId: "",
@@ -26,14 +45,115 @@ export default class Register extends Component {
             message: "",
             successful: false
         });
+    }
 
+    onChangeRoleId(e) {
+        this.setState({
+            roleId: e.target.value
+        });
+    }
 
+    onChangeFirstName(e) {
+        this.setState({
+            firstName: e.target.value
+        });
+    }
+
+    onChangeLastName(e) {
+        this.setState({
+            lastName: e.target.value
+        });
+    }
+
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
     }
 
     render() {
         return(
             <div className="col-md-12">
+                <Form
+                    onSubmit={this.handleRegister}
+                    ref={c => {
+                        this.form = c;
+                    }}
+                >
+                    <div className="container">
+                        <div className="form-group">
+                            <label htmlFor="roleId">Role ID:</label>
+                            <Input
+                                type="number"
+                                className="form-control"
+                                name="roleId"
+                                value={this.state.roleId}
+                                onChange={this.onChangeRoleId}
+                                validations={[required]}
+                            />
+                        </div>
 
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name:</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="firstName"
+                                value={this.state.firstName}
+                                onChange={this.onChangeFirstName}
+                                validations={[required]}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="lastName">Last Name:</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="lastName"
+                                value={this.state.lastName}
+                                onChange={this.onChangeLastName}
+                                validations={[required]}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email">Email (username):</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.onChangeEmail}
+                                validations={[required]}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password:</label>
+                            <Input
+                                type="password"
+                                className="form-control"
+                                name="password1"
+                                value={this.state.password}
+                                onChange={this.onChangePassword}
+                                validations={[required]}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <Button className="btn btn-primary btn-block">Register</Button>
+                        </div>
+
+
+                    </div>
+                </Form>
             </div>
         );
     }
