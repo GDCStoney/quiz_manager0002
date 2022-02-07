c2fb48fb918d8a364906d28b80b2dc1fa0d3b79c
gstoney2 <gstoney2@ford.com>
2022-02-07 16:29:24 +0000

    Changing the App to a class (needed later) and also including the HOME functionality, so test passes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,41 +1,60 @@
+import React, {Component} from "react";
+import {Link, Route, Routes} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import {Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to={"/"} className="navbar-brand">
-          Quiz Manager
-        </Link>
-        <div className="navbar-nav me-auto">
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Home
+import Home from "./Home/Home";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  render() {
+    return (
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <Link to={"/"} className="navbar-brand">
+              Quiz Manager
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Manage Quizzes
-            </Link>
-          </li>
+            <div className="navbar-nav me-auto">
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">
+                  Manage Quizzes
+                </Link>
+              </li>
+            </div>
+            <div className="navbar-nav mt-auto">
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">
+                  Register
+                </Link>
+              </li>
+            </div>
+          </nav>
+
+          <div className="container mt-3">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+            </Routes>
+          </div>
         </div>
-        <div className="navbar-nav mt-auto">
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Login
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Register
-            </Link>
-          </li>
-        </div>
-      </nav>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
