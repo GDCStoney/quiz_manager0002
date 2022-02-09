567862acc168b73063dcf17704a9e39b4c7bc267
gstoney2 <gstoney2@ford.com>
2022-02-09 09:15:41 +0000

    Added the Manager holding component to show available quizzes

---
+++ src/Components/Manager/ManagerComponent.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+export default class ManagerComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: ""
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        )
+    }
+}
