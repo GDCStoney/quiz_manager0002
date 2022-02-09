a8dee5619b93135c476afa45645532c1e9354576
gstoney2 <gstoney2@ford.com>
2022-02-09 09:21:53 +0000

    Added the Quiz List service functionality, includes authHeader for calls to protected API resources

--- src/Services/quiz-service.js
+++ src/Services/quiz-service.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 
+import authHeader from "./auth-header";
+
+
 const API_URL = "http://localhost:8080/api/quizzes";
 
-class quizService {
+class QuizService {
 
     getQuizList() {
-
+        return axios({
+            method: 'get',
+            url: API_URL + 'quizzes',
+            headers: authHeader()
+            });
     }
 
 }
 
-export default new quizService();
+export default new QuizService();
