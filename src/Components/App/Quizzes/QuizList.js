2e7202aa60d6b540bf4e54b247d17bcf651754c6
gstoney2 <gstoney2@ford.com>
2022-02-09 09:16:35 +0000

    Added the Quiz List service and holding component for getting the quiz list and putting in a container for the Manager component

---
+++ src/Components/App/Quizzes/QuizList.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+export default class QuizList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            contents: ""
+        };
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
---
+++ src/Services/quiz-service.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/quizzes";
+
+class quizService {
+
+    getQuizList() {
+
+    }
+
+}
+
+export default new quizService();
