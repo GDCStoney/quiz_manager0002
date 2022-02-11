1a1d1be7a7b06ccc2c9003c0d52addea27b7e2da
gstoney2 <gstoney2@ford.com>
2022-02-11 13:41:29 +0000

    Creation of the Service call to the Question API to get a list of questions

---
+++ src/Services/question-service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import authHeader from "./auth-header";
+
+
+const API_BASE_URL = "http://localhost:8080/api/quizzes/";
+const API_QUESTIONS_URL = '/questions'
+
+class QuestionService {
+
+    getQuestionList(quizId) {
+        return axios({
+            method: 'get',
+            url: API_BASE_URL + quizId + API_QUESTIONS_URL,
+            headers: authHeader()
+        });
+    }
+
+}
+
+export default new QuestionService();
