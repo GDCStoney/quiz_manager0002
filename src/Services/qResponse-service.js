98885e8cb07e3aa7920851fd367b29d093f1a278
gstoney2 <gstoney2@ford.com>
2022-02-11 16:19:53 +0000

    corrections to the endpoint builder for qResponse-service.js

--- src/Services/qResponse-service.js
+++ src/Services/qResponse-service.js
@@ -4,11 +4,13 @@ import authHeader from "./auth-header";
 
 
 const API_BASE_URL = "http://localhost:8080/api/quizzes/";
-const API_QUESTIONS_URL = '/questions';
+const API_QUESTIONS_URL = '/questions/';
 const API_QRESPONSES_URL = '/qresponses';
 
 class QResponseService {
     getQResponseList(quizId, questionId) {
+        console.log(quizId);
+        console.log(questionId);
         return axios({
             method: 'get',
             url: API_BASE_URL + quizId + API_QUESTIONS_URL + questionId + API_QRESPONSES_URL,
