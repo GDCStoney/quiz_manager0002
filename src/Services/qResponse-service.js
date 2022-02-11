2a0b352407587390961b16bb9785bc462640f001
gstoney2 <gstoney2@ford.com>
2022-02-11 15:55:55 +0000

    Added QResponses service functionality for getting question responses from the API

---
+++ src/Services/qResponse-service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+import authHeader from "./auth-header";
+
+
+const API_BASE_URL = "http://localhost:8080/api/quizzes/";
+const API_QUESTIONS_URL = '/questions';
+const API_QRESPONSES_URL = '/qresponses';
+
+class QResponseService {
+    getQResponseList(quizId, questionId) {
+        return axios({
+            method: 'get',
+            url: API_BASE_URL + quizId + API_QUESTIONS_URL + questionId + API_QRESPONSES_URL,
+            headers: authHeader()
+        });
+    }
+}
+
+export default new QResponseService;
