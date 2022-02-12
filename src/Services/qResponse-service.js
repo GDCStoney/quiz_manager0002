a395b5b4b385418cf3348a35b0a8fd7ff4dacb50
gstoney2 <gstoney2@ford.com>
2022-02-12 19:46:42 +0000

    Added initial placeholders for QResponse API functionality for SETTER role

--- src/Services/qResponse-service.js
+++ src/Services/qResponse-service.js
@@ -15,6 +15,32 @@ class QResponseService {
             headers: authHeader()
         });
     }
+
+    addQResponse(qResponse) {
+        return axios({
+            method: 'post',
+            url: API_BASE_URL + question.quizId + API_QUESTIONS_URL + qResponse.questionId + API_QRESPONSES_URL,
+            data: { responseText: qResponse.responseText, correctAnswer: qResponse.correctAnswer },
+            headers: authHeader()
+        });
+    }
+
+    updateQResponse(qResponse) {
+        return axios({
+            method: 'put',
+            url: API_BASE_URL + qResponse.quizId + API_QUESTIONS_URL + qResponse.questionId + API_QRESPONSES_URL + "/" + qResponse.qResponseId,
+            data: { responseText: qResponse.responseText, correctAnswer: qResponse.correctAnswer },
+            headers: authHeader()
+        });
+    }
+
+    deleteQResponse(qResponse) {
+        return axios({
+            method: 'delete',
+            url: API_BASE_URL + qResponse.quizId + API_QUESTIONS_URL + qResponse.questionId + API_QRESPONSES_URL + "/" + qResponse.qResponseId,
+            headers: authHeader()
+        });
+    }
 }
 
 export default new QResponseService();
